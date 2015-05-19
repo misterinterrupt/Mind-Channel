978cefa1e07dc9bc0db9c5f6c6d7fb8345256ef0
Matt Howell <misterinterrupt@gmail.com>
2015-05-19 13:52:10 -0700

    data can now be read from and written to the evernote api

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,8 @@
 // }
 var app = (function app() {
   
-  var config = {};
+  var config = {},
+      options = {};
 
   // Bind Event Listeners
   //
@@ -43,7 +44,7 @@ var app = (function app() {
       options = {
           consumerKey: config.consumerKey,
           consumerSecret: config.consumerSecret,
-          callbackUrl : "gotOAuth.html", // this filename doesn't matter in this example
+          callbackUrl : "redirect.html", // this filename doesn't matter in this example
           signatureMethod : "HMAC-SHA1"
       };
       oauth = OAuth(options);
@@ -52,85 +53,95 @@ var app = (function app() {
   };
   var success = function success(data) {
     var isCallBackConfirmed = false;
-    var token = '';
-    var vars = data.text.split("&");
-    for (var i = 0; i < vars.length; i++) {
+      var tempToken = '';
+      var vars = data.text.split("&");
+      for (var i = 0; i < vars.length; i++) {
         var y = vars[i].split('=');
         if(y[0] === 'oauth_token')  {
-            token = y[1];
+          tempToken = y[1];
         }
         else if(y[0] === 'oauth_token_secret') {
-            this.oauth_token_secret = y[1];
-            localStorage.setItem("oauth_token_secret", y[1]);
+          this.oauth_token_secret = y[1];
+          localStorage.setItem("oauth_token_secret", y[1]);
         }
         else if(y[0] === 'oauth_callback_confirmed') {
-            isCallBackConfirmed = true;
+          isCallBackConfirmed = true;
         }
-    }
-    var ref;
-    if(isCallBackConfirmed) {
-        // step 2
-        ref = window.open(config.evernoteHostName + '/OAuth.action?oauth_token=' + token, '_blank', 'location=no');
-        ref.addEventListener('loadstart', function(event) { console.log('start: ' + event.url); }); 
-        ref.addEventListener('loadstop', function(event) { console.log('stop: ' + event.url); }); 
-        ref.addEventListener('loaderror', function(event) { console.log('error: ' + event.message); }); 
-        ref.addEventListener('exit', function(event) { console.log(event.type); });
-        ref.addEventListener('loadstart',
-          function(event) {
-            var loc = event.url;
-            if (loc.indexOf('gotOAuth.html?') >= 0) {
-              var index, verifier = '';
-              var got_oauth = '';
-              var params = loc.substr(loc.indexOf('?') + 1);
-              params = params.split('&');
-              for (var i = 0; i < params.length; i++) {
-                var y = params[i].split('=');
-                if(y[0] === 'oauth_verifier') {
-                  verifier = y[1];
-                }
-              }
-            } else if(y[0] === 'gotOAuth.html?oauth_token') {
-              got_oauth = y[1];
-            }
-            // step 3
-            oauth.setVerifier(verifier);
-            oauth.setAccessToken([got_oauth, localStorage.getItem("oauth_token_secret")]);
+      }
+      var ref;
+      if(isCallBackConfirmed) {
+         // step 2
+         ref = window.open(config.evernoteHostName + '/OAuth.action?oauth_token=' + tempToken, '_blank');
+         ref.addEventListener('loadstart', function(event) {
+                              //alert(event.type + ' - ' + event.url);
+                              var loc = event.url;
+                              if (loc.indexOf('/redirect.html?') > 0) {
+                                    var index, verifier = '';
+                                    var got_oauth = '';
+                                    var params = loc.substr(loc.indexOf('?') + 1);
+                                    params = params.split('&');
+                                    for (var i = 0; i < params.length; i++) {
+                                        var y = params[i].split('=');
+                                        if(y[0] === 'oauth_verifier') {
+                                            verifier = y[1];
+                                        }
+                                        else if(y[0] === 'oauth_token') {
+                                            got_oauth = y[1];
+                                        }
+                                    }
+                              // step 3
+                              oauth.setVerifier(verifier);
+                              oauth.setAccessToken([got_oauth, localStorage.getItem("oauth_token_secret")]);
+                              
+                              var getData = { 'oauth_verifier': verifier };
+                              ref.close();
+                              delete ref;
+                              oauth.request({'method': 'GET', 'url': config.evernoteHostName + '/oauth',
+                                            'success': success, 'failure': failure});
 
-            var getData = {'oauth_verifier': verifier};
-            ref.close();
-            oauth.request({'method': 'GET', 'url': config.evernoteHostName + '/oauth',
-              'success': app.success, 'failure': app.failure});
-          }
-        );
-    } else {
-        // Step 4 : Get the final token
-        var querystring = app.getQueryParams(data.text);
-        var authTokenEvernote = querystring.oauth_token;
-        // authTokenEvernote can now be used to send request to the Evernote Cloud API
-        
-        // Here, we connect to the Evernote Cloud API and get a list of all of the
-        // notebooks in the authenticated user's account:
-        var noteStoreURL = querystring.edam_noteStoreUrl;
-        var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
-        var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
-        var noteStore = new NoteStoreClient(noteStoreProtocol);
-        noteStore.listNotebooks(authTokenEvernote, function (notebooks) {
-            console.log(notebooks);
-          },
-          function onerror(error) {
-              console.log(error);
-          });
-        var note = new Note;
-        note.content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\"><en-note><span style=\"font-weight:bold;\">Hello photo note.</span><br /><span>Evernote logo :</span><br /></en-note>";
-        note.title = "Hello javascript lib";
-        noteStore.createNote(authTokenEvernote,note,function (noteCallback) {
-          console.log(noteCallback.guid + " created");
-        });
-    }
+                              }
+                              });
+         
+      } else {
+         var querystring = getQueryParams(data.text);
+         var noteStoreURL = querystring.edam_noteStoreUrl;
+         var noteStoreTransport = new Thrift.BinaryHttpTransport(noteStoreURL);
+         var noteStoreProtocol = new Thrift.BinaryProtocol(noteStoreTransport);
+         var noteStore = new NoteStoreClient(noteStoreProtocol);
+         var authTokenEvernote = querystring.oauth_token; 
+         noteStore.listNotebooks(authTokenEvernote, function (notebooks) {
+                                 console.log(notebooks);
+                                 },
+                                 function onerror(error) {
+                                 console.log(error);
+                                 });
+         var note = new Note;
+         note.content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\"><en-note><span style=\"font-weight:bold;\">Hello photo note.</span><br /><span>Evernote logo :</span><br /></en-note>";
+         note.title = "Hello javascript lib";
+         noteStore.createNote(authTokenEvernote,note,function (noteCallback) {
+                              console.log(noteCallback.guid + " created");
+                              });
+      }
+       
   };
   var failure = function failure(error) {
       console.log('error ' + error.text);
   };
+  var getQueryParams = function getQueryParams(queryParams) {
+    var i, query_array,
+    query_array_length, key_value, decode = OAuth.urlDecode,querystring = {};
+    // split string on '&'
+    query_array = queryParams.split('&');
+    // iterate over each of the array items
+    for (i = 0, query_array_length = query_array.length; i < query_array_length; i++) {
+      // split on '=' to get key, value
+      key_value = query_array[i].split('=');
+      if (key_value[0] != "") {
+        querystring[key_value[0]] = decode(key_value[1]);
+      }
+    }
+    return querystring;
+  }
 
   // return scope
   return {
